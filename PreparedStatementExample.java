a74f0981a42acbb86b2a5c56e98026494c55d95f
DanielMevs <dannymevs@hotmail.com>
2021-08-05 12:50:54 -0400

    Add files via upload

--- PreparedStatementExample.java
+++ PreparedStatementExample.java
@@ -19,12 +19,13 @@ public class PreparedStatementExample {
 				System.out.println("Enter f for freshmen, sp for sophomore, j for junior or sn for senior");
 				Scanner input = new Scanner(System.in);
 				String classYear = input.nextLine();
-				int size = 0;
-				ResultSet rs = pstmt.executeQuery();
+				int count = 0;
 				switch(classYear) {
 				case "f":
 					pstmt.setInt(1, 0);
 					pstmt.setInt(2, 30);
+					ResultSet rs = pstmt.executeQuery();
+					count = 0;
 					System.out.println("Freshmen/man");
 					System.out.println("-----------------");
 					while(rs.next()) {
@@ -33,13 +34,10 @@ public class PreparedStatementExample {
 						int creds = rs.getInt("credits");
 						System.out.println("Name: " + firstName + " " + lastName +
 								"    Creadits: " + creds);
+						count++;
 					}
-					size = 0;
-					if(rs != null) {
-						rs.last();
-						size = rs.getRow();
-					}
-					System.out.println("\nTotal: " + size);
+				
+					System.out.println("\nTotal: " + count);
 					
 					
 					rs.close();
@@ -47,69 +45,67 @@ public class PreparedStatementExample {
 				case "sp":
 					pstmt.setInt(1,  30);
 					pstmt.setInt(2, 60);
+					ResultSet rs1= pstmt.executeQuery();
+					count = 0;
 					System.out.println("Sophomore(s)");
 					System.out.println("-----------------");
-					while(rs.next()) {
-						String firstName = rs.getString("first_name");
-						String lastName = rs.getString("last_name");
-						int creds = rs.getInt("credits");
+					while(rs1.next()) {
+						String firstName = rs1.getString("first_name");
+						String lastName = rs1.getString("last_name");
+						int creds = rs1.getInt("credits");
 						System.out.println("Name: " + firstName + " " + lastName +
 								"    Creadits: " + creds);
+						count++;
 					}
-					size = 0;
-					if(rs != null) {
-						rs.last();
-						size = rs.getRow();
-					}
-					System.out.println("\nTotal: " + size);
 					
-					rs.close();
+					System.out.println("\nTotal: " + count);
+					
+					rs1.close();
 
 					break;
 					
 				case "j":
 					pstmt.setInt(1,  60);
 					pstmt.setInt(2, 90);
+					ResultSet rs2 = pstmt.executeQuery();
+					count = 0;
 					System.out.println("Junior(s)");
 					System.out.println("-----------------");
-					while(rs.next()) {
-						String firstName = rs.getString("first_name");
-						String lastName = rs.getString("last_name");
-						int creds = rs.getInt("credits");
+					while(rs2.next()) {
+						String firstName = rs2.getString("first_name");
+						String lastName = rs2.getString("last_name");
+						int creds = rs2.getInt("credits");
 						System.out.println("Name: " + firstName + " " + lastName +
 								"    Creadits: " + creds);
+						count++;
 					}
-					size = 0;
-					if(rs != null) {
-						rs.last();
-						size = rs.getRow();
-					}
-					System.out.println("\nTotal: " + size);					
-					rs.close();
+					
+					System.out.println("\nTotal: " + count);					
+					rs2.close();
 					break;
 					
 				case "sn":
 					pstmt.setInt(1, 90);
-					pstmt.setDouble(2, Double.POSITIVE_INFINITY);
+					pstmt.setDouble(2, Integer.MAX_VALUE);
+					ResultSet rs3 = pstmt.executeQuery();
+					count=0;
 					System.out.println("Senior(s)");
 					System.out.println("-----------------");
-					while(rs.next()) {
-						String firstName = rs.getString("first_name");
-						String lastName = rs.getString("last_name");
-						int creds = rs.getInt("credits");
+					while(rs3.next()) {
+						String firstName = rs3.getString("first_name");
+						String lastName = rs3.getString("last_name");
+						int creds = rs3.getInt("credits");
 						System.out.println("Name: " + firstName + " " + lastName +
 								"    Creadits: " + creds);
+						count++;
 					}
-					size = 0;
-					if(rs != null) {
-						rs.last();
-						size = rs.getRow();
-					}
-					System.out.println("\nTotal: " + size);					
-					rs.close();
+
+					System.out.println("\nTotal: " + count);					
+					rs3.close();
 					break;
 				default:
-					System.out.println("That is not a valid response.");
+					System.out.println("That is not a valid option.");
+				
 				}
 				System.out.println("\nContinue? Enter Y or y for yes and anything else for no");
 				interested = input.next().charAt(0);
